bbc979cdb37c4321b37590c73d49d3af9632965c
Shobhit Pandey <Shobhitpandey8@gmail.com>
2017-12-01 14:50:27 +0530

    Project init

--- urls.py
+++ urls.py
@@ -12,7 +12,7 @@ Class-based views
 Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
-""
+"""
 from django.conf.urls import url
 from django.contrib import admin
 
