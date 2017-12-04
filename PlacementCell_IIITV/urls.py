50311736e97765a50678f491e40b0ce2605a6fac
praneethsa1 <201551044@iiitvadodara.ac.in>
2017-12-04 21:57:15 +0530

    Added css and js to homepage

--- PlacementCell_IIITV/urls.py
+++ PlacementCell_IIITV/urls.py
@@ -18,10 +18,12 @@ from django.conf.urls import url, include
 from django.conf.urls.static import static
 from django.contrib import admin
 
+import MyWebsite
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^', include('MyWebsite.urls',namespace='mywebsite')),
-]+ static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+    url(r'^', include('MyWebsite.urls', namespace="mywebsite")),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL,     document_root=settings.MEDIA_URL)
