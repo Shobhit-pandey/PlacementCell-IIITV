11cc6dbe84b10980e3da1dd065c5903460699f27
anjansrivathsav <anjansrivathsav1997@gmail.com>
2017-12-09 03:26:34 +0530

    added sending mail system after filling form

--- PlacementCell_IIITV/settings.py
+++ PlacementCell_IIITV/settings.py
@@ -29,6 +29,14 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'studymonk.se@gmail.com'
+EMAIL_HOST_PASSWORD = 'qwertyuiopzxcvbnm'
+EMAIL_PORT = 587
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+PHONENUMBER_DEFAULT_REGION = 'IN'
 
 # Application definition
 
@@ -133,3 +141,4 @@ STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
+LOGIN_REDIRECT_URL = '/'
