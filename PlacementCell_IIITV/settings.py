fcafabd1c52557dd9c2954b2be6bd942e2f0843c
shivam <201551087@iiitvadodara.ac.in>
2017-12-09 06:33:52 +0530

    Merge branch 'master' of https://github.com/Shobhit-pandey/PlacementCell-IIITV
    # Conflicts:
    #	MyWebsite/templates/Recruiter.html
    #	MyWebsite/templates/Student.html
    #	MyWebsite/templates/recruiterform.html
    #	MyWebsite/urls.py

--- PlacementCell_IIITV/settings.py
+++ PlacementCell_IIITV/settings.py
@@ -29,6 +29,14 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'studymonk.se@gmail.com'
+EMAIL_HOST_PASSWORD = 'qwertyuiopzxcvbnm'
+EMAIL_PORT = 587
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+PHONENUMBER_DEFAULT_REGION = 'IN'
 
 # Application definition
 
