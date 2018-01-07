80ffa043b335561d1621ec55b2fd679d14ee4d9e
Shobhit Pandey <Shobhitpandey8@gmail.com>
2018-01-08 01:05:25 +0530

    minor changes

--- resume/views.py
+++ resume/views.py
@@ -12,12 +12,16 @@ from django.views.decorators.csrf import csrf_protect
 from django.views.generic import ListView
 
 from resume.forms import ProjectFormset, ResumeForm, OtherFormset
+from resume.models import Resume
 
-def resume(request,pk2):
+
+def resume(request, pk2):
     users = User.objects.filter(username=pk2)
     return render(request, 'student_resume.html', {'users': users})
 
+
 def createresume(request, pk):
+    resumes = Resume.objects.filter(user_id=request.user.id)
     referer = request.META.get('HTTP_REFERER')
     if referer == None:
         return render(request, 'quiz/wrongurl.html')
@@ -25,25 +29,25 @@ def createresume(request, pk):
     other_formset = OtherFormset()
     resume_form = ResumeForm(initial={'user_id': request.user.id})
     if request.method == "POST":
-            print("-3")
-            resume_form = ResumeForm(request.POST, initial={'user_id': request.user.id})
-            if resume_form.is_valid():
-                print("-4")
-                resume_form.cleaned_data['user_id'] = request.user.id
-                new_resume = resume_form.save()
-                new_resume.save()
-                print("1")
-                project_formset = ProjectFormset(request.POST, instance=new_resume)
-                other_formset = OtherFormset(request.POST, instance=new_resume)
-                if project_formset.is_valid() and other_formset.is_valid():
-                    print("2")
-                    project_formset.save()
-                    other_formset.save()
-                    return redirect('mywebsite:student')
+        print("-3")
+        resume_form = ResumeForm(request.POST, initial={'user_id': request.user.id})
+        if resume_form.is_valid():
+            print("-4")
+            resume_form.cleaned_data['user_id'] = request.user.id
+            new_resume = resume_form.save()
+            new_resume.save()
+            print("1")
+            project_formset = ProjectFormset(request.POST, instance=new_resume)
+            other_formset = OtherFormset(request.POST, instance=new_resume)
+            if project_formset.is_valid() and other_formset.is_valid():
+                print("2")
+                project_formset.save()
+                other_formset.save()
+                return redirect('mywebsite:student')
     else:
         resume_form = ResumeForm(request.POST, initial={'user_id': request.user.id})
         project_formset = ProjectFormset()
         other_formset = OtherFormset()
     return render(request, 'create_resume.html',
                   {'resume_form': resume_form, 'project_formset': project_formset,
-                   'other_formset': other_formset,})
+                   'other_formset': other_formset, 'resumes': resumes})
