cfce58463eca6babf4416db6ca548a1e7d467c36
Shobhit Pandey <Shobhitpandey8@gmail.com>
2018-01-10 00:37:38 +0530

    add update and delete in resume

--- resume/urls.py
+++ resume/urls.py
@@ -3,6 +3,8 @@ from django.conf.urls import patterns, url
 from resume import views
 
 urlpatterns = [
+    url(r'^resume/delete/$',views.delete_resume,name='delete_resume'),
     url(r'^create-resume/(?P<pk>[\w\-]+)/$',views.createresume,name='createresume'),
     url(r'^resume/(?P<pk2>[\w\-]+)/$',views.resume,name='resume'),
+
 ]
--- resume/views.py
+++ resume/views.py
@@ -8,16 +8,31 @@ from django.contrib.auth.models import User
 from django.core.mail import EmailMessage
 from django.core.urlresolvers import reverse
 from django.shortcuts import render, redirect
+from django.template import RequestContext
 from django.views.decorators.csrf import csrf_protect
 from django.views.generic import ListView
 
 from resume.forms import ProjectFormset, ResumeForm, OtherFormset
-from resume.models import Resume
+from resume.models import Resume, Project, Other
 
 
 def resume(request, pk2):
     users = User.objects.filter(username=pk2)
     return render(request, 'student_resume.html', {'users': users})
+
+
+def delete_resume(request):
+    resume = Resume.objects.filter(user_id=request.user.id)
+    list = []
+    for resu in resume:
+        list.append(resu.id)
+    resume.delete()
+    for x in list:
+        projects = Project.objects.filter(resume_id=x)
+        other = Other.objects.filter(resume_id=x)
+        projects.delete()
+        other.delete()
+    return render(request, 'delete_resume.html')
 
 
 def createresume(request, pk):
@@ -29,18 +44,14 @@ def createresume(request, pk):
     other_formset = OtherFormset()
     resume_form = ResumeForm(initial={'user_id': request.user.id})
     if request.method == "POST":
-        print("-3")
         resume_form = ResumeForm(request.POST, initial={'user_id': request.user.id})
         if resume_form.is_valid():
-            print("-4")
             resume_form.cleaned_data['user_id'] = request.user.id
             new_resume = resume_form.save()
             new_resume.save()
-            print("1")
             project_formset = ProjectFormset(request.POST, instance=new_resume)
             other_formset = OtherFormset(request.POST, instance=new_resume)
             if project_formset.is_valid() and other_formset.is_valid():
-                print("2")
                 project_formset.save()
                 other_formset.save()
                 return redirect('mywebsite:student')
@@ -50,4 +61,5 @@ def createresume(request, pk):
         other_formset = OtherFormset()
     return render(request, 'create_resume.html',
                   {'resume_form': resume_form, 'project_formset': project_formset,
-                   'other_formset': other_formset, 'resumes': resumes})
+                   'other_formset': other_formset, 'resumes': resumes,},
+                  context_instance=RequestContext(request))
